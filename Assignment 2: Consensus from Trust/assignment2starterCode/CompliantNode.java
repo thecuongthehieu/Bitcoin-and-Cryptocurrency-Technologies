1da926293f87b3f224d90b3194ec869b9357701c
thecuongthehieu <thecuongthehieu@gmail.com>
2025-10-04 23:53:49 +0700

    Solution of 78/100 points for Assignment 2

--- Assignment 2: Consensus from Trust/assignment2starterCode/CompliantNode.java
+++ Assignment 2: Consensus from Trust/assignment2starterCode/CompliantNode.java
@@ -1,26 +1,59 @@
-import java.util.ArrayList;
-import java.util.Set;
+import java.util.*;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+    private final int numRounds;
+
+    private int roundCount;
+
+    private boolean[] followees;
+    private final Map<Transaction, Integer> txFreqMap;
+    private final Set<Transaction> sentTxSet;
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         // IMPLEMENT THIS
+        this.roundCount = 0;
+        this.numRounds = numRounds;
+
+        this.followees = new boolean[0];
+        this.txFreqMap = new HashMap<>();
+        this.sentTxSet = new HashSet<>();
     }
 
     public void setFollowees(boolean[] followees) {
         // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
         // IMPLEMENT THIS
+        for (Transaction transaction : pendingTransactions) {
+            this.txFreqMap.put(transaction, 1);
+        }
     }
 
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
+        this.roundCount += 1;
+        if (this.roundCount > this.numRounds) {
+            return this.txFreqMap.keySet();
+        }
+
+        Set<Transaction> res = new HashSet<>();
+        for (Transaction transaction : this.txFreqMap.keySet()) {
+            if (!this.sentTxSet.contains(transaction)) {
+                res.add(transaction);
+                this.sentTxSet.add(transaction);
+            }
+        }
+
+        return res;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
         // IMPLEMENT THIS
+        for (Candidate candidate : candidates) {
+            this.txFreqMap.put(candidate.tx, this.txFreqMap.getOrDefault(candidate.tx, 0) + 1);
+        }
     }
 }
---
+++ Assignment 2: Consensus from Trust/assignment2starterCode/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Step 1: Clean old .class files
+#echo "Cleaning old class files..."
+rm -f *.class
+
+# Step 2: Compile all .java files
+#echo "Compiling Java files..."
+javac *.java
+
+# Step 3: Run the main class
+#echo "Running program..."
+java Simulation 0.1 0.15 0.01 10
+
